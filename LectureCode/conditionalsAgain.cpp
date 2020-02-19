3f79bf99b823e5bee0030ed3118641cf72917bf5
Justin Wilson <justinwilson502@yahoo.com>
2020-02-19 14:22:18 -0800

    added folder and new assignment

---
+++ LectureCode/conditionalsAgain.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 8;
+    if (4 < 3)
+    {
+        cout<<"I made it!";
+    }
+    if(100==9)
+    {
+        cout<<"This is nonesense";
+    }
+    else if (100 == 9)
+    {
+        cout<<"This is nonesense";
+    }
+    
+    else
+    {
+        cout<<"Didn't make it ;-;";
+    }
+    
+    
+
+    return 0;
+}
