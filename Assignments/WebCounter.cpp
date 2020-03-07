2b5192949fe44f889512d55c7529d0f02d392b20
Justin Wilson <justinwilson502@yahoo.com>
2020-03-07 00:38:49 -0800

    added WebCounter.cpp

---
+++ Assignments/WebCounter.cpp
@@ -0,0 +1,45 @@
+/*************************************************************
+[PROGRAM NAME]
+Author: [YOUR NAME]
+Date Created: [DATE]
+Description: [PROGRAM DESCRIPTION]
+*************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+// Implement WebCounter class definition here
+
+
+int main()
+{
+
+    WebCounter wc(42);
+
+    cout<<"Web Counter Tester:"<<endl<<endl;
+    cout<<"Start: "<<wc.get()<<endl;
+    
+    wc.hit();
+    wc.hit();
+    
+    cout<<"Two Hits: "<<wc.get()<<endl;
+
+    wc.hit();
+    
+    cout<<"Three Hits: "<<wc.get()<<endl;
+
+    wc.reset();
+
+    cout<<"Reset: "<<wc.get()<<endl;
+
+
+    wc.hit();
+    wc.hit();
+    wc.hit();
+    wc.hit();
+
+    cout<<"Four Hits: "<<wc.get()<<endl;
+
+    return 0;
+}
